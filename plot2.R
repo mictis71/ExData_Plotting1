689eaf520e03d43163747fd588edbe919c1eca9d
Michael Tissot <michaeltissot71@gmail.com>
2014-08-09 21:54:20 +1000

    read for submission
    enabled data reading and tested

--- plot2.R
+++ plot2.R
@@ -6,7 +6,19 @@
 
 plot2 <- function(){
 	plotpng <- "plot2.png"
-	df<-getData()	
+
+	# read data
+	df<-read.table("household_power_consumption.txt", header = TRUE
+			, sep = ";", na.strings = c("NA", "?"))
+	
+	## tidy data convert & combine date & time
+	df$Date <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
+	
+	## subset data
+	from <- as.POSIXct("2007-02-01 00:00")
+	to <- as.POSIXct("2007-02-03 00:00")
+	df <- df[df$Date > from & df$Date < to,]
+	
 	## Create Histogram
 	png(filename = plotpng, width = 480, height = 480)
 	with(df, plot(Date, Global_active_power, type = "l", 
--- plot4.R
+++ plot4.R
@@ -5,7 +5,18 @@
 
 plot4 <- function(){
 	plotpng <- "plot4.png"
-	df<-getData()	
+
+	# read data
+	df<-read.table("household_power_consumption.txt", header = TRUE
+			, sep = ";", na.strings = c("NA", "?"))
+	
+	## tidy data convert & combine date & time
+	df$Date <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
+	
+	## subset data
+	from <- as.POSIXct("2007-02-01 00:00")
+	to <- as.POSIXct("2007-02-03 00:00")
+	df <- df[df$Date > from & df$Date < to,]
 	
 	## Create Histograms
 	png(filename = plotpng, width = 480, height = 480)
